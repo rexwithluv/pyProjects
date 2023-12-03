28f5842573934cee134b6beff7d32eddc606daae
rexwithluv <114663850+rexwithluv@users.noreply.github.com>
2023-12-03 16:07:05 +0100

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -17,7 +17,7 @@ valid_letters = string.ascii_uppercase
 used_letters = []
 
 # word = input("Give me a word: ").upper()
-word = "GOGO"
+word = "TEST"
 word_dct = str_for_dct(word)
 shadow = ["_" for i in range(len(word))]
 
@@ -27,7 +27,10 @@ while "_" in shadow:
     letter = input("Give me a letter: ").upper()
 
     if letter in word and letter in valid_letters:
-        for i in dct.get(letter):
+        used_letters.append(letter)
+        for i in word_dct.get(letter):
             shadow[i] = letter
+    else:
+        print(f"The {letter} isn`t in the word")
 
 print(f"Congratulations! The word is {word}")
